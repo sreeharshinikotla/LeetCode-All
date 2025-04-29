68cafdd0e969d1db10f055defd096920e8902e31
Sree Harshini <149524440+sreeharshinikotla@users.noreply.github.com>
2025-04-28 21:08:26 -0500

    Time: 2 ms (93%), Space: 43.4 MB (24.48%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        HashSet<Integer> doubl= new HashSet<>();
-        HashSet<Integer> half= new HashSet<>();
-        for(int i=0; i< arr.length; i++){
-            if(doubl.contains(arr[i]) || half.contains(arr[i])) return true;
-            else{
-                doubl.add(arr[i]*2);
-               if(arr[i]%2==0) half.add(arr[i]/2);
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            if(map.containsKey(arr[i]*2) || (arr[i]%2==0 && map.containsKey(arr[i]/2))){
+                return true;
             }
+            map.put(arr[i],i);
         }
         return false;
     }
