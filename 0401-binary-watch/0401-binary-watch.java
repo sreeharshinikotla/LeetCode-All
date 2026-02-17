b7bddf3fc3fa5473145d6bcce024a9fc3834e850
Sree <149524440+sreeharshinikotla@users.noreply.github.com>
2026-02-16 17:11:18 -0800

    Time: 14 ms (8.18%), Space: 49.1 MB (53.62%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+
+        for (int hour = 0; hour < 12; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
+
+                int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
+
+                if (totalBits == turnedOn) {
+                    result.add(hour + ":" + String.format("%02d", minute));
+                }
+            }
+        }
+
+        return result;
+    }
+}
